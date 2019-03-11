f0248c93b9697d9768da4408e5ad8fb5a7c0436a
raoyuandong <837215079@qq.com>
2019-03-11 09:23:08 +0800

    调试

--- resources/views/web/article/detail.blade.php
+++ resources/views/web/article/detail.blade.php
@@ -229,4 +229,7 @@
 
     commentObj.loadCommentData();
 </script>
+<link href="http://cdn.bootcss.com/highlight.js/8.0/styles/monokai_sublime.min.css" rel="stylesheet">
+<script src="http://cdn.bootcss.com/highlight.js/8.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 @endpush
